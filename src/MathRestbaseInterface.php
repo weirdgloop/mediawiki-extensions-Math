061b7d94da7c4b380cbeaa84910987554b05b087
TehKittyCat <tehkittycat@weirdgloop.org>
2023-10-25 12:09:38 -0400

    Revert "Remove references to VisualEditorFullRestbaseURL"
    This reverts commit 8c05a71bebfabc78c4d1c228ab9ef93528387a52.

--- src/MathRestbaseInterface.php
+++ src/MathRestbaseInterface.php
@@ -205,13 +205,16 @@ class MathRestbaseInterface {
 	 * Case A: <code>$internal = false</code>, which means one needs a URL that is accessible from
 	 * outside:
 	 *
-	 * --> Use <code>$wgMathFullRestbaseURL</code>. It must always be configured.
+	 * --> If <code>$wgMathFullRestbaseURL</code> is configured use it, otherwise fall back try to
+	 * <code>$wgVisualEditorFullRestbaseURL</code>. (Note, that this is not be worse than failing
+	 * immediately.)
 	 *
-	 * Case B: <code>$internal = true</code>, which means one needs to access content from Restbase
+	 * Case B: <code> $internal= true</code>, which means one needs to access content from Restbase
 	 * which does not need to be accessible from outside:
 	 *
-	 * --> Use the mount point when it is available and <code>$wgMathUseInternalRestbasePath =
-	 * true</code>. If not, use <code>$wgMathFullRestbaseURL</code>.
+	 * --> Use the mount point when it is available and <code> $wgMathUseInternalRestbasePath=
+	 * true</code>. If not, use <code>$wgMathFullRestbaseURL</code> with fallback to
+	 * <code>wgVisualEditorFullRestbaseURL</code>
 	 *
 	 * @param string $path
 	 * @param bool|true $internal
