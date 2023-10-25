89b65f8b011cedf37c018ce6c25b065b985642fc
TehKittyCat <tehkittycat@weirdgloop.org>
2023-10-25 12:13:43 -0400

    Revert "Introduce MathInternalRestbaseURL setting."
    This reverts commit 43dd9c7f637e3b71ad1ed4caab03950a4e4fb1d0.

--- src/Hooks.php
+++ src/Hooks.php
@@ -10,12 +10,10 @@ namespace MediaWiki\Extension\Math;
 
 // phpcs:disable MediaWiki.NamingConventions.LowerCamelFunctionsName.FunctionName
 
-use ConfigException;
 use ExtensionRegistry;
 use Maintenance;
 use MediaWiki\Hook\MaintenanceRefreshLinksInitHook;
 use MediaWiki\MediaWikiServices;
-use MediaWiki\Settings\SettingsBuilder;
 use MediaWiki\SpecialPage\Hook\SpecialPage_initListHook;
 use RequestContext;
 
@@ -23,69 +21,6 @@ class Hooks implements
 	SpecialPage_initListHook,
 	MaintenanceRefreshLinksInitHook
 {
-
-	/**
-	 * Extension registration callback, used to apply dynamic defaults for configuration variables.
-	 */
-	public static function onConfig( array $extInfo, SettingsBuilder $settings ) {
-		$config = $settings->getConfig();
-
-		$fullRestbaseUrl = $config->get( 'MathFullRestbaseURL' );
-		$internalRestbaseURL = $config->get( 'MathInternalRestbaseURL' );
-		$useInternalRestbasePath = $config->get( 'MathUseInternalRestbasePath' );
-		$virtualRestConfig = $config->get( 'VirtualRestConfig' );
-
-		if ( !$fullRestbaseUrl ) {
-			if ( $config->has( 'VisualEditorFullRestbaseURL' ) ) {
-				$settings->warning( "MathFullRestbaseURL is falling back to using VisualEditorFullRestbaseURL. " .
-					"Please configure the Mathoid API URL explicitly." );
-
-				$fullRestbaseUrl = $config->get( 'VisualEditorFullRestbaseURL' );
-				$settings->overrideConfigValue( 'MathFullRestbaseURL', $fullRestbaseUrl );
-			} else {
-				throw new ConfigException(
-					'Math extension can not find Restbase URL. Please specify $wgMathFullRestbaseURL.'
-				);
-			}
-		}
-
-		if ( !$useInternalRestbasePath ) {
-			if ( $internalRestbaseURL ) {
-				$settings->warning( "The MathInternalRestbaseURL setting will be ignored " .
-					"because MathUseInternalRestbasePath is set to false." );
-			}
-
-			// Force the use of the external URL for internal calls as well.
-			$settings->overrideConfigValue( 'MathInternalRestbaseURL', $fullRestbaseUrl );
-		} elseif ( !$internalRestbaseURL ) {
-			if ( isset( $virtualRestConfig['modules']['restbase'] ) ) {
-				$settings->warning( "The MathInternalRestbaseURL is falling back to " .
-					"VirtualRestConfig. Please set MathInternalRestbaseURL explicitly." );
-
-				$restBaseUrl = $virtualRestConfig['modules']['restbase']['url'];
-				$restBaseUrl = rtrim( $restBaseUrl, '/' );
-
-				$restBaseDomain = $virtualRestConfig['modules']['restbase']['domain'] ?? 'localhost';
-
-				// Ensure the correct domain format: strip protocol, port,
-				// and trailing slash if present.  This lets us use
-				// $wgCanonicalServer as a default value, which is very convenient.
-				// XXX: This was copied from RestbaseVirtualRESTService. Use UrlUtils::parse instead?
-				$restBaseDomain = preg_replace(
-					'/^((https?:)?\/\/)?([^\/:]+?)(:\d+)?\/?$/',
-					'$3',
-					$restBaseDomain
-				);
-
-				$internalRestbaseURL = "$restBaseUrl/$restBaseDomain/v1/";
-			} else {
-				// Default to using the external URL for internal calls as well.
-				$internalRestbaseURL = $fullRestbaseUrl;
-			}
-
-			$settings->overrideConfigValue( 'MathInternalRestbaseURL', $internalRestbaseURL );
-		}
-	}
 
 	/**
 	 * MaintenanceRefreshLinksInit handler; optimize settings for refreshLinks batch job.
