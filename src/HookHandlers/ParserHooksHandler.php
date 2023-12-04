daf0257c70eef985346c3f3fef6bfac5459f98a3
TehKittyCat <tehkittycat@weirdgloop.org>
2023-12-04 06:04:04 -0500

    Revert "Update UserOptionsLookup's FQN"
    This reverts commit 2b2f48cd6965e8057a13ad9c262a5e40c64374a6.

--- src/HookHandlers/ParserHooksHandler.php
+++ src/HookHandlers/ParserHooksHandler.php
@@ -13,7 +13,7 @@ use MediaWiki\Hook\ParserFirstCallInitHook;
 use MediaWiki\Hook\ParserOptionsRegisterHook;
 use MediaWiki\HookContainer\HookContainer;
 use MediaWiki\Logger\LoggerFactory;
-use MediaWiki\User\Options\UserOptionsLookup;
+use MediaWiki\User\UserOptionsLookup;
 use Parser;
 use ParserOptions;
 
